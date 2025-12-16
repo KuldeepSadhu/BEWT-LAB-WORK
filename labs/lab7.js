9d7f6b646b3f62c3032384402af9db98ece24c16
Sadhu Kuldeep <kuladeepsadhu932@gmail.com>
2025-12-16 05:31:03 +0000

    lab 7

--- labs/lab7.js
+++ labs/lab7.js
@@ -5,15 +5,22 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.write("Home Page")
+            res.end()
     }
     if (req.url === '/about') {
         res.write("about Page")
+            res.end()
+
     }
     if (req.url === '/contact') {
         res.write("contact Page")
+            res.end()
+
     }
     if (req.url === '/profile') {
         res.write("profile Page")
+            res.end()
+
     }
     if (req.url === '/edits') {
 
@@ -36,7 +43,6 @@ const server = http.createServer((req, res) => {
 
         })
     }
-    res.end()
 })
 
 server.listen(PORT, () => {
